bb76400747bc82e3d33314a85c3ff9066ca4b76c
Abhi <abhiraja1012@gmail.com>
2020-03-29 03:40:30 -0400

    remove old imports

--- app/src/main/java/com/opensourcebrothers/covidkwtracker/MainActivity.java
+++ app/src/main/java/com/opensourcebrothers/covidkwtracker/MainActivity.java
@@ -1,27 +1,11 @@
+
 package com.opensourcebrothers.covidkwtracker;
 
 import android.arch.lifecycle.ViewModelProviders;
-import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG_LIST_FRAGMENT = "TAG_LIST_FRAGMENT";
