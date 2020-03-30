25ebd94cf4cf8aaae0a10908835264cee52c6300
Abhi <abhiraja1012@gmail.com>
2020-03-30 01:01:50 -0400

    add divider line in recycler item

--- app/src/main/java/com/opensourcebrothers/covidkwtracker/PatientListFragment.java
+++ app/src/main/java/com/opensourcebrothers/covidkwtracker/PatientListFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -42,7 +43,11 @@ public class PatientListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Context context = view.getContext();
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+        LinearLayoutManager layoutManager=new LinearLayoutManager(context);
+        mRecyclerView.setLayoutManager(layoutManager);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),
+                layoutManager.getOrientation());
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
         mRecyclerView.setAdapter(mPatientAdapter);
     }
 
